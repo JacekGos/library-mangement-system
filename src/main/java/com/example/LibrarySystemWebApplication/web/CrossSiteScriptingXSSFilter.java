472052175e43538bd77f1c7f12f375d2a45f8b05
Jacekg93 <jacekg93@gmail.com>
2021-08-05 10:47:52 +0200

    Added: XSS filter to main servlet

---
+++ src/main/java/com/example/LibrarySystemWebApplication/web/CrossSiteScriptingXSSFilter.java
@@ -0,0 +1,22 @@
+package com.example.LibrarySystemWebApplication.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = " CrossSiteScriptingXSSFilter", value = "/")
+public class CrossSiteScriptingXSSFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        chain.doFilter(new CrossSiteScriptingXSSRequestWrapper((HttpServletRequest) request), response);
+    }
+}
